74460776132f1b6fb08caa0699420f875c1de490
Devin Neal <devin@eulertour.com>
2019-08-31 23:24:35 -0700

    add timeline and controls

---
+++ src/components/VideoControls.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="d-flex justify-center mt-3">
+    <v-btn fab v-on:click="$emit('step-backward')" class="mx-2">
+      <v-icon color="black" x-large>mdi-step-backward</v-icon>
+    </v-btn>
+    <v-btn fab v-if="playing" v-on:click="$emit('pause')" class="mx-4">
+      <v-icon color="black" x-large>mdi-pause</v-icon>
+    </v-btn>
+    <v-btn fab v-else v-on:click="$emit('play')" class="mx-4">
+      <v-icon color="black" x-large>mdi-play</v-icon>
+    </v-btn>
+    <v-btn fab v-on:click="$emit('step-forward')" class="mx-2">
+      <v-icon color="black" x-large>mdi-step-forward</v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VideoControls',
+  components: {
+
+  },
+  props: {
+    scene: Object,
+  },
+  computed: {
+    playing: function() {
+      return this.scene !== null && this.scene.playing;
+    }
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  mounted() {
+
+  },
+  watch: {
+
+  }
+}
+</script>
+
+<style scoped>
+</style>
