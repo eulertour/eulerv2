e381ac04907bbe830eb207979385ed234a681686
Safin Singh <42120904+safinsingh@users.noreply.github.com>
2020-03-31 13:33:55 -0700

    Create documentation page

---
+++ src/components/docs/Docs.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="display-1 my-4">Documentation</div>
+    <p>
+      Manim runs by adding Mobjects to a scene, and using them to animate certain actions defined by the user. First, let's take a look at how to start a manim program.
+    </p>
+
+    <p>
+      First, you need to create a new python file. Then, enter these basic components: 
+      <Prism language="python" code=
+"from manimlib.imports import *
+
+Class Test(Scene):
+    def construct(self):"
+			/>
+        You'll enter your code in the <code>construct</code> block.
+    </p>
+  </div>
+</template>
+
+<script>
+import Prism from 'vue-prism-component'
+export default {
+	name: 'Docs',
+	components: { Prism },
+}
+</script>
+
